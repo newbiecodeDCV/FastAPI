9dea653d3b623ecb8f7ffbfa04908798a85c4e73
newbiecodeDVC <nguyentrungtrieu080705@gmai.com>
2024-06-28 10:37:01 +0700

    update

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 from typing import Union
-from  fastapi import FastAPI
-
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+class ModelName(str,Enum):
+    alexnet="alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 @app.get("/")
 def read_root():
-    return  {"Hello":"word"}
+    return {"Hello": "World"}
 
-@app.get("/item/{item_id}")
-def read_item(item_id:int,q : Union[str,None] = None):
-    return {"item_id":item_id,"q":q}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/items/20")
+def read_item1():
+    return {"test":20}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
